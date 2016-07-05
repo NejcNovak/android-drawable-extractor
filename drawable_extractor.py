6bc80f4d0b22cac3ffde8b523a35aee5ed376c74
nejcnovak <nejc.novak92@gmail.com>
2016-07-05 17:05:41 +0200

    frames with medium padding

--- drawable_extractor.py
+++ drawable_extractor.py
@@ -13,3 +13,29 @@ class MyCheckButton(Checkbutton):
 
     def is_checked(self):
         return self.var.get()
+
+class Window:
+    frame_padding = 10
+
+    def __init__(self, master):
+        self.folder = ""
+        self.file = ""
+        self.drawables = []
+
+        # FRAMES
+        self.folder_select_frame = Frame(root)
+        self.file_select_frame = Frame(root)
+        self.checkbutton_frame = Frame(root)
+        self.footer_frame = Frame(root)
+        self.setupFrames()
+
+    def setupFrames(self):
+        self.folder_select_frame.pack(fill=X, pady=self.frame_padding, padx=self.frame_padding)
+        self.file_select_frame.pack(fill=X, pady=self.frame_padding, padx=self.frame_padding)
+        self.checkbutton_frame.pack(fill=X, pady=self.frame_padding, padx=self.frame_padding)
+        self.footer_frame.pack(fill=X, pady=self.frame_padding, padx=self.frame_padding)
+
+root = Tk()
+window = Window(root)
+root.wm_title("Drawable Extractor")
+root.mainloop()
