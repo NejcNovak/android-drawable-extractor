94727417d8c9e7f23bb9a8f96663edce87b4177c
nejcnovak <nejc.novak92@gmail.com>
2016-07-05 19:31:48 +0200

    show statuses when used incorrectly

--- drawable_extractor.py
+++ drawable_extractor.py
@@ -52,21 +52,22 @@ class Window:
         # FILE SELECT FRAME
         self.file_helper_text = Label(self.file_select_frame,
                                       text='Select the file containing the names of drawables you wish to export')
-        self.file_label = Label(self.file_select_frame, text="Selector File")
+        self.file_label = Label(self.file_select_frame, text='Selector File')
         self.file_name_input = Entry(self.file_select_frame)
-        self.browse_file_button = Button(self.file_select_frame, text="Browse", command=self.browse_file)
-        self.use_file_button = Button(self.file_select_frame, text="Use File", command=self.check_file)
+        self.browse_file_button = Button(self.file_select_frame, text='Browse', command=self.browse_file)
+        self.use_file_button = Button(self.file_select_frame, text='Use File', command=self.check_file)
 
         # CHECKBUTTON FRAME
-        self.checkbutton_label = Label(self.checkbutton_frame, text="Select the drawables you wish to export")
-        self.vsb = Scrollbar(self.checkbutton_frame, orient="vertical")
+        self.checkbutton_label = Label(self.checkbutton_frame, text='Select the drawables you wish to export')
+        self.vsb = Scrollbar(self.checkbutton_frame, orient='vertical')
         self.text = Text(self.checkbutton_frame, width=40, height=20, yscrollcommand=self.vsb.set)
-        self.clear_all = Button(self.checkbutton_frame, text="Clear All", command=self.clear_all)
+        self.clear_all = Button(self.checkbutton_frame, text='Clear All', command=self.clear_all)
 
         # FOOTER FRAME
-        self.output_folder_label = Label(self.footer_frame, text="Output Folder Name")
+        self.success_label = Label(self.footer_frame)
+        self.output_folder_label = Label(self.footer_frame, text='Output Folder Name')
         self.output_folder_name_input = Entry(self.footer_frame)
-        self.extract_button = Button(self.footer_frame, text="Extract Drawables", command=self.extract)
+        self.extract_button = Button(self.footer_frame, text='Extract Drawables', command=self.extract)
 
     def setup_frames(self):
         self.folder_select_frame.pack(fill=X, pady=self.frame_padding, padx=self.frame_padding)
@@ -96,17 +97,20 @@ class Window:
         self.text.pack(side=LEFT, fill=BOTH, expand=True)
 
     def setup_footer_frame(self):
+        self.success_label.pack(side=BOTTOM)
         self.output_folder_label.pack(side=LEFT)
         self.output_folder_name_input.pack(side=LEFT)
         self.extract_button.pack(side=LEFT)
 
     def browse_folder(self):
+        self.success_label['text'] = ''
         Tk().withdraw()
         self.folder = askdirectory()
         self.folder_name_input.delete(0, END)
         self.folder_name_input.insert(0, self.folder)
 
     def browse_file(self):
+        self.success_label['text'] = ''
         Tk().withdraw()
         self.file = askopenfilename()
         self.file_name_input.delete(0, END)
@@ -114,13 +118,14 @@ class Window:
 
     def check_folder(self):
         folder = self.folder_name_input.get()
-        hdpi_folder = folder + "/drawable-hdpi"
+        hdpi_folder = folder + '/drawable-hdpi'
 
         if exists(hdpi_folder) and isdir(hdpi_folder):
             self.setup_file_selector_frame()
             self.setup_checkbutton_frame()
             self.draw_checkboxes(hdpi_folder)
             self.setup_footer_frame()
+            self.folder_help_text['text'] = 'Select the folder containing all the drawable folders (usually "res")'
         else:
             self.folder_help_text['text'] = 'Selected incorrect folder. Please select again.'
 
@@ -129,8 +134,9 @@ class Window:
 
         if exists(filename) and isfile(filename):
             self.check_from_file(filename)
-        else:
             self.file_helper_text['text'] = 'Selected incorrect file. Please select again.'
+        else:
+            self.file_helper_text['text'] = 'Select the file containing the names of drawables you wish to export'
 
     def draw_checkboxes(self, folder):
         all_files = [f for f in listdir(folder) if isfile(join(folder, f))]
@@ -138,14 +144,14 @@ class Window:
         for i, drawable in enumerate(all_files):
             check = MyCheckButton(self.checkbutton_frame, text=drawable)
             self.checkbuttons.append(check)
-            self.drawables.append(drawable.split(".")[0])
-            self.text.window_create("end", window=check)
-            self.text.insert("end", "\n")  # force one checkbox per line
+            self.drawables.append(drawable.split('.')[0])
+            self.text.window_create('end', window=check)
+            self.text.insert('end', '\n')  # force one checkbox per line
 
     def check_from_file(self, filename):
         with open(filename) as f:
             for line in f:
-                line = line.replace("\n", "")
+                line = line.replace('\n', '')
                 if line in self.drawables:
                     self.checkbuttons[self.drawables.index(line)].select()
 
@@ -154,11 +160,23 @@ class Window:
             c.deselect()
 
     def extract(self):
-        output_name = self.folder + '/' + self.output_folder_name_input.get()
-        self.create_folders(output_name)
-        for i, checkbutton in enumerate(self.checkbuttons):
-            if checkbutton.is_checked():
-                self.copy_files(checkbutton.get_text())
+        if self.no_checkbuttons_checked():
+            self.success_label['text'] = "Please select at least one drawable you wish to extract."
+        elif self.output_folder_name_input.get().strip() == '':
+            self.success_label['text'] = "Please select a name for the output folder."
+        else:
+            output_name = self.folder + '/' + self.output_folder_name_input.get()
+            self.create_folders(output_name)
+            for i, checkbutton in enumerate(self.checkbuttons):
+                if checkbutton.is_checked():
+                    self.copy_files(checkbutton.get_text())
+            self.success_label['text'] = "Extraction of the selected drawables was successful."
+
+    def no_checkbuttons_checked(self):
+        for c in self.checkbuttons:
+            if c.is_checked():
+                return False
+        return True
 
     def create_folders(self, parent_folder_name):
         self.parent_folder_name = self.create_parent_folder(parent_folder_name)
@@ -181,9 +199,7 @@ class Window:
             if exists(self.folder + density + text):
                 copyfile(self.folder + density + text, self.parent_folder_name + density + text)
 
-
-
 root = Tk()
 window = Window(root)
-root.wm_title("Android Drawable Extractor")
+root.wm_title('Android Drawable Extractor')
 root.mainloop()
